906c86ae43b375a4bdbac8fc9c1feae65d2fe56c
Marco <marco-bm@arcor.de>
2016-11-14 22:41:40 +0100

    added two feature files

--- TheLearningTriangle/src/de/thelearningtriangle/opengl/core/Application.java
+++ TheLearningTriangle/src/de/thelearningtriangle/opengl/core/Application.java
@@ -2,13 +2,13 @@ package de.thelearningtriangle.opengl.core;
 
 public class Application
 {
-    
-    public static void main(String[] args)
-    {
-        Game game = new Game();
-        game.registerDrawableFigure(new LearningTriangleFigure(0.4f, 0.3f));
-        game.registerDrawableFigure(new LearningTriangleFigure(0.1f, -0.4f));
-        game.registerDrawableFigure(new LearningTriangleFigure(-0.2f, -0.1f));
-    }
-    
+
+	public static void main(String[] args)
+	{
+		Game game = new Game();
+		game.registerDrawableFigure(new LearningTriangleFigure(0.4f, 0.3f));
+		game.registerDrawableFigure(new LearningTriangleFigure(0.1f, -0.4f));
+		game.registerDrawableFigure(new LearningTriangleFigure(-0.2f, -0.1f));
+	}
+
 }
