255f5a9e23545be7a43b1d6862e38b1b5a41823f
Steven Kovacs <Kovacs.S2@ok.de>
2017-05-31 15:25:33 +0200

    Refactored TriangleOverworldFactory

--- TheLearningTriangle/src/de/thelearningtriangle/core/overworld/TriangleOverworldFactory.java
+++ TheLearningTriangle/src/de/thelearningtriangle/core/overworld/TriangleOverworldFactory.java
@@ -1,61 +1,78 @@
 package de.thelearningtriangle.core.overworld;
 
 import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import de.thelearningtriangle.core.overworld.field.AbstractField;
 import de.thelearningtriangle.core.overworld.field.FieldType;
 
-public class TriangleOverworldFactory {
+public class TriangleOverworldFactory
+{
 	private static Random random;
-
-	public static TriangleOverworld generateOverworld(int size, Random random) {
+	
+	public static TriangleOverworld generateOverworld(int size, Random random)
+	{
 		TriangleOverworldFactory.random = random;
-
+		
 		TriangleOverworld triangleOverworld = new TriangleOverworld(random);
-
+		
 		triangleOverworld.setMap(generateField(size));
-
+		
 		return triangleOverworld;
 	}
-
-	private static AbstractField[][] generateField(int size) {
-
-		AbstractField[][] field = generateRandomField(size);
-
+	
+	private static AbstractField[][] generateField(int size)
+	{
+		
+		AbstractField[][] field = generateRandomMapMatrixOf(size);
+		
 		setBorder(field);
-
+		
 		return field;
 	}
-
-	private static AbstractField[][] generateRandomField(int size) {
-		AbstractField[][] field = new AbstractField[size][size];
-
-		for (int j = 0; j < field.length; j++) {
-			for (int i = 0; i < field[j].length; i++) {
-				field[j][i] = FieldType.getRandomFieldType(random).newInstance();
-			}
-		}
+	
+	private static AbstractField[][] generateRandomMapMatrixOf(int size)
+	{
+		AbstractField[][] field = IntStream.range(0, size)
+				.mapToObj(unusedBuffer -> generateRandomMapVectorOf(size))
+				.collect(Collectors.toList())
+				.toArray(new AbstractField[0][0]);
 		return field;
 	}
-
-	private static void setBorder(AbstractField[][] field) {
+	
+	private static AbstractField[] generateRandomMapVectorOf(int size)
+	{
+		return IntStream.range(0, size)
+				.mapToObj(unusedBuffer -> FieldType.getRandomFieldType(random))
+				.map(FieldType::newInstance)
+				.collect(Collectors.toList())
+				.toArray(new AbstractField[0]);
+	}
+	
+	private static void setBorder(AbstractField[][] field)
+	{
 		int size = field.length;
 		field[0] = getWallVector(size);
 		field[size - 1] = getWallVector(size);
 		setSideFieldsToWallsWith(field);
 	}
-
-	private static void setSideFieldsToWallsWith(AbstractField[][] field) {
+	
+	private static void setSideFieldsToWallsWith(AbstractField[][] field)
+	{
 		int size = field.length;
-		for (int i = 1; i < size - 1; i++) {
+		for (int i = 1; i < size - 1; i++)
+		{
 			field[i][0] = FieldType.WALL.newInstance();
 			field[i][size - 1] = FieldType.WALL.newInstance();
 		}
 	}
-
-	private static AbstractField[] getWallVector(int size) {
+	
+	private static AbstractField[] getWallVector(int size)
+	{
 		AbstractField[] wallVector = new AbstractField[size];
-		for (int i = 0; i < wallVector.length; i++) {
+		for (int i = 0; i < wallVector.length; i++)
+		{
 			wallVector[i] = FieldType.WALL.newInstance();
 		}
 		return wallVector;
