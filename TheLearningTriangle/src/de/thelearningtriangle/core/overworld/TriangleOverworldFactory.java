1c7bd95c3212f006d72611f5495d6b59361c9a90
KingMus <marcofm47@gmail.com>
2017-11-15 13:11:11 +0100

    removed old training classes
    and other unnecessary stuff

--- TheLearningTriangle/src/de/thelearningtriangle/core/overworld/TriangleOverworldFactory.java
+++ TheLearningTriangle/src/de/thelearningtriangle/core/overworld/TriangleOverworldFactory.java
@@ -8,76 +8,62 @@ import java.util.stream.IntStream;
 import de.thelearningtriangle.core.overworld.field.AbstractField;
 import de.thelearningtriangle.core.overworld.field.FieldType;
 
-public class TriangleOverworldFactory
-{
+public class TriangleOverworldFactory {
 	private static Random random;
-	
-	//variables for triangle spawnpoint
+
+	// variables for triangle spawnpoint
 	private static int triangleX;
 	private static int triangleY;
-	
-	public static TriangleOverworld generateOverworld(int worldSize, Random random)
-	{
+
+	public static TriangleOverworld generateOverworld(int worldSize, Random random) {
 		TriangleOverworldFactory.random = random;
-		
+
 		TriangleOverworld triangleOverworld = new TriangleOverworld(random);
-		
+
 		AbstractField[][] worldMap = generateField(worldSize);
 		triangleOverworld.setMap(worldMap);
-		
-//		TriangleOverworldFileLoader.writeFileFromMap(worldMap);
-		
+
+		// TriangleOverworldFileLoader.writeFileFromMap(worldMap);
+
 		return triangleOverworld;
 	}
-	
-	private static AbstractField[][] generateField(int worldSize)
-	{
+
+	private static AbstractField[][] generateField(int worldSize) {
 		AbstractField[][] field = generateRandomMapMatrixOf(worldSize);
 		setBorder(field);
 		return field;
 	}
-	
-	private static AbstractField[][] generateRandomMapMatrixOf(int worldSize)
-	{
+
+	private static AbstractField[][] generateRandomMapMatrixOf(int worldSize) {
 		AbstractField[][] field = IntStream.range(0, worldSize)
-				.mapToObj(unusedBuffer -> generateRandomMapVectorOf(worldSize))
-				.collect(Collectors.toList())
+				.mapToObj(unusedBuffer -> generateRandomMapVectorOf(worldSize)).collect(Collectors.toList())
 				.toArray(new AbstractField[0][0]);
 		return field;
 	}
-	
-	private static AbstractField[] generateRandomMapVectorOf(int worldSize)
-	{
-		return IntStream.range(0, worldSize)
-				.mapToObj(unusedBuffer -> FieldType.getRandomFieldType(random))
-				.map(FieldType::newInstance)
-				.collect(Collectors.toList())
-				.toArray(new AbstractField[0]);
+
+	private static AbstractField[] generateRandomMapVectorOf(int worldSize) {
+		return IntStream.range(0, worldSize).mapToObj(unusedBuffer -> FieldType.getRandomFieldType(random))
+				.map(FieldType::newInstance).collect(Collectors.toList()).toArray(new AbstractField[0]);
 	}
-	
-	private static void setBorder(AbstractField[][] field)
-	{
+
+	private static void setBorder(AbstractField[][] field) {
 		int size = field.length;
 		field[0] = getWallVector(size);
 		field[size - 1] = getWallVector(size);
 		setSideFieldsToWallsWith(field);
 	}
-	
-	private static void setSideFieldsToWallsWith(AbstractField[][] field)
-	{
+
+	private static void setSideFieldsToWallsWith(AbstractField[][] field) {
 		int size = field.length;
-		for (int i = 1; i < size - 1; i++)
-		{
+		for (int i = 1; i < size - 1; i++) {
 			field[i][0] = FieldType.WALL.newInstance();
 			field[i][size - 1] = FieldType.WALL.newInstance();
 		}
 	}
-	
-	private static AbstractField[] getWallVector(int size)
-	{
+
+	private static AbstractField[] getWallVector(int size) {
 		AbstractField[] wallVector = new AbstractField[size];
-		for (int i = 0; i < wallVector.length; i++)
-		{
+		for (int i = 0; i < wallVector.length; i++) {
 			wallVector[i] = FieldType.WALL.newInstance();
 		}
 		return wallVector;
@@ -85,42 +71,41 @@ public class TriangleOverworldFactory
 
 	/**
 	 * loads an overworld-String from an file and converts it into a map
+	 * 
 	 * @param mapData
 	 * @param random
 	 * @author Marco Mueller
 	 * @return an usable TriangleOverworld triangleOverworld
-	 * @throws FieldAccessException 
-	 * @throws NoMapException 
+	 * @throws FieldAccessException
+	 * @throws NoMapException
 	 */
-	public static TriangleOverworld loadOverworld(List<String[]> mapData) throws NoMapException, FieldAccessException
-	{
+	public static TriangleOverworld loadOverworld(List<String[]> mapData) throws NoMapException, FieldAccessException {
 
 		TriangleOverworld triangleOverworld = new TriangleOverworld(random);
-		
+
 		AbstractField[][] worldMap = convertStringMapToFieldMap(triangleOverworld, mapData);
-		
+
 		triangleOverworld.setMap(worldMap);
-		
+
 		return triangleOverworld;
 	}
 
 	/**
 	 * converts the String mapData into an usable map
+	 * 
 	 * @param mapData
 	 * @author Marco Mueller
-	 * @param triangleOverworld 
+	 * @param triangleOverworld
 	 * @return AbstractField[][] worldMap
 	 */
-	private static AbstractField[][] convertStringMapToFieldMap(TriangleOverworld triangleOverworld, List<String[]> mapData)
-	{
+	private static AbstractField[][] convertStringMapToFieldMap(TriangleOverworld triangleOverworld,
+			List<String[]> mapData) {
 		AbstractField[][] worldMap = new AbstractField[mapData.size()][mapData.size()];
-		
-		for (int i = 0; i < mapData.size(); i++)
-		{
-			for (int j = 0; j < worldMap.length; j++)
-			{
-				switch (mapData.get(j)[i])
-				{
+
+		for (int i = 0; i < mapData.size(); i++) {
+			for (int j = 0; j < worldMap.length; j++) {
+
+				switch (mapData.get(j)[i]) {
 				case "9":
 					triangleX = i;
 					triangleY = j;
@@ -145,19 +130,18 @@ public class TriangleOverworldFactory
 					worldMap[i][j] = FieldType.POISON.newInstance();
 					break;
 				}
+
 			}
 		}
-		
+
 		return worldMap;
 	}
 
-	public static int getTriangleX()
-	{
+	public static int getTriangleX() {
 		return triangleX;
 	}
 
-	public static int getTriangleY()
-	{
+	public static int getTriangleY() {
 		return triangleY;
 	}
 }
